873fb6a3a9fedbddeeb3687ca171fa2e8c8a8511
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2023-10-06 11:01:55 +0200

    [backend][configuration] java spring setup

---
+++ finted-app/src/main/java/it/unical/demacs/pierluigi/fintedapp/FintedAppApplication.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.pierluigi.fintedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintedAppApplication.class, args);
+	}
+
+}
---
+++ finted-app/src/test/java/it/unical/demacs/pierluigi/fintedapp/FintedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.pierluigi.fintedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
