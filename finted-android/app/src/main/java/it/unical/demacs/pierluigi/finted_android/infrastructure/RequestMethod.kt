d776814e2d1be4940040a1b85ed956ce48e46a1b
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2023-10-19 15:16:18 +0200

    [frontend][android] http setup

---
+++ finted-android/app/src/main/java/it/unical/demacs/pierluigi/finted_android/infrastructure/RequestMethod.kt
@@ -0,0 +1,8 @@
+package it.unical.demacs.pierluigi.finted_android.infrastructure
+
+/**
+ * Provides enumerated HTTP verbs
+ */
+enum class RequestMethod {
+    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
+}
